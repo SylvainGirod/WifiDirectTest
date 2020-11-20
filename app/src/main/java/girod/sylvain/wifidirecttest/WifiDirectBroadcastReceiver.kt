b31ab8a3ad6e18cf630ce29310c54d17773f2606
Sylvain GIROD <sylvain@keyops.tech>
2020-11-20 10:26:40 +0100

    Init

---
+++ app/src/main/java/girod/sylvain/wifidirecttest/WifiDirectBroadcastReceiver.kt
@@ -0,0 +1,39 @@
+package girod.sylvain.wifidirecttest
+
+import android.annotation.SuppressLint
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.net.wifi.p2p.WifiP2pDeviceList
+import android.net.wifi.p2p.WifiP2pManager
+import android.util.Log
+
+class WifiDirectBroadcastReceiver(
+    private val manager: WifiP2pManager,
+    private val channel: WifiP2pManager.Channel,
+    private val activity: MainActivity
+) : BroadcastReceiver() {
+    @SuppressLint("MissingPermission")
+    override fun onReceive(context: Context?, intent: Intent?) {
+        requireNotNull(context)
+        requireNotNull(intent)
+        when (intent.action) {
+            WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                when (state) {
+                    WifiP2pManager.WIFI_P2P_STATE_ENABLED -> {
+                        // Wifi P2P is enabled
+                    }
+                    else -> {
+                        // Wifi P2P is disabled
+                    }
+                }
+            }
+            WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                manager.requestPeers(channel) { peers: WifiP2pDeviceList? ->
+                    Log.d("BBLOG", peers?.deviceList.toString())
+                }
+            }
+        }
+    }
+}
